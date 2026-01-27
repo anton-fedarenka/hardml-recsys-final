6306541dba7208906733e1aa92cc7881642cdb0e
Anton Fedorenko <fedarenka.anton@gmail.com>
2026-01-27 15:55:39 +0300

    New files and structure rearrangement for docker packing

---
+++ bandit.py
@@ -0,0 +1,34 @@
+import numpy as np
+from typing import List
+from dataclasses import dataclass
+
+@dataclass
+class ThompsonSamplingBandit():
+    alpha: List[float] # = field(default_factory = lambda: np.zeros(n_arms) + init_alpha)
+    beta:  List[float] #= field(default_factory = lambda: np.zeros(n_arms) + init_beta)
+    alpha_weight: float = 1.
+    beta_weight: float = 1.
+        
+    def sample(self) -> int:
+        return np.argmax(
+            np.random.beta(
+                self.alpha_weight * np.array(self.alpha), 
+                self.beta_weight * np.array(self.beta)
+            )
+        )
+
+    def get_top_indices(self, k: int = 10) -> List[int]:
+        beta_distr_array = np.random.beta(
+            self.alpha_weight * np.array(self.alpha), 
+            self.beta_weight * np.array(self.beta)
+        )
+        top_n_inds = np.argpartition(beta_distr_array, -k)[-k:]
+        top_n_vals = beta_distr_array[top_n_inds]
+        return top_n_inds[np.argsort(top_n_vals)[::-1]].tolist() 
+    
+    def retrieve_reward(self, arm_ind: int, action: str, n: int) -> None:
+        if action == 'like':
+            self.alpha[arm_ind] += n
+        else:
+            self.beta[arm_ind] += n
+        return
